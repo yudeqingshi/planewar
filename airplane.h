779fb176d9fe48ff1bff0fcbc6ef96dea897c997
yudeqingshi <1162969538@qq.com>
2026-05-07 13:30:37 +0800

    添加碰撞检测和计分逻辑

--- airplane.h
+++ airplane.h
@@ -8,21 +8,20 @@
 #include <conio.h>
 #include "list.h"
 
-// 窗口大小
 #define bgWidth 400
 #define bgHeight 600
 
-// 我方飞机大小
 #define myairWidth 30
 #define myairHeight 45
 
-// 子弹大小
 #define bulletWidth 8
 #define bulletHeight 8
 
-// 敌机大小
 #define enemyWidth 25
 #define enemyHeight 25
+
+#define bigEnemyWidth 100
+#define bigEnemyHeight 100
 
 extern IMAGE img[5];
 extern IMAGE mask[5];
@@ -42,12 +41,17 @@ void initMyPlane(MyPlane* plane);
 void moveMyPlane(MyPlane* plane);
 void drawMyPlane(MyPlane* plane);
 
-void generateEnemy(LL* enemyList, int spawnRate = 50, int minSpeed = 2, int maxSpeed = 4);
+void generateEnemy(LL* enemyList, int spawnRate = 50, int minSpeed = 2, int maxSpeed = 4, bool spawnBigEnemy = false);
 void drawEnemy(LL* enemyList);
 void updateEnemy(LL* enemyList);
 
 void fireBullet(MyPlane* plane, LL* bulletList, int* lastFireTime);
 void drawBullet(LL* bulletList);
 void updateBullet(LL* bulletList);
+
+bool checkCollision(int x1, int y1, int w1, int h1,
+                    int x2, int y2, int w2, int h2);
+
+void handleCollision(LL* bulletList, LL* enemyList, MyPlane* plane, int* score);
 
 void start();
