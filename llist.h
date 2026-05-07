99d69c017889b600ab4060b327e521876bd9073b
yudeqingshi <1162969538@qq.com>
2026-05-07 13:30:09 +0800

    增加大敌机生命值设计

--- llist.h
+++ llist.h
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <assert.h>
 
-// 链表节点结构体，用于管理子弹和敌机
+// 链表节点结构体
 typedef struct NODE
 {
     int x;
     int y;
     int speed;
+    int life;
+    bool isBigEnemy;
     struct NODE* next;
 } Node;
 
@@ -18,20 +20,9 @@ typedef struct LinkList
     struct NODE* end;
 } LL;
 
-// 创建节点
 Node* Node_init(int x, int y, int speed);
-
-// 初始化链表
 LL* LinkList_init();
-
-// 尾插法插入节点
 void LinkList_insert(LL* list, int x, int y, int speed);
-
-// 删除指定节点
 void LinkList_delete(LL* list, Node* node);
-
-// 更新链表节点位置
 void LinkList_update(LL* list, int direction);
-
-// 销毁链表
 void LinkList_destroy(LL* list);
