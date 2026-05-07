241449f213a36528a7cc3fbebf38705e5e8d53d8
yudeqingshi <1162969538@qq.com>
2026-05-07 13:26:45 +0800

    加入玩家飞机结构体，并实现飞机初始化、移动、绘制接口

--- airplane.h
+++ airplane.h
@@ -11,15 +11,25 @@
 #define bgWidth 400
 #define bgHeight 600
 
-// 图片数组
+// 我方飞机大小
+#define myairWidth 30
+#define myairHeight 45
+
 extern IMAGE img[5];
 extern IMAGE mask[5];
 
-// 图片透明处理
+// 我方飞机结构体
+typedef struct MyPlane
+{
+    int x;
+    int y;
+    int speed;
+    bool alive;
+} MyPlane;
+
 void makeTransparent(IMAGE* src, IMAGE* maskImg);
-
-// 游戏初始化
 void init();
-
-// 游戏启动
+void initMyPlane(MyPlane* plane);
+void moveMyPlane(MyPlane* plane);
+void drawMyPlane(MyPlane* plane);
 void start();
